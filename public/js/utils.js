6530639883c670581846a1376d270d75bd8c7dd5
Filipa-Freitas <filipafreitascontact@gmail.com>
2021-08-05 19:02:20 +0200

    add showsucces() showerror() et checkInput() form validation

--- public/js/utils.js
+++ public/js/utils.js
@@ -78,70 +78,127 @@ async function sendFormData(url, data) {
 async function handleFormSubmit(e) {
     e.preventDefault();
 
-    const formError = document.createElement('p');
-    formError.innerText = 'Veuillez remplir les champs indiqués';
-    formError.classList = 'error';
+    const firstName = document.getElementById('first-name')
+    const lastName = document.getElementById('last-name')
+    const address = document.getElementById('address')
+    const city = document.getElementById('city')
+    const email = document.getElementById('email')
 
-    const firstName = document.getElementById('first-name').value
-    const lastName = document.getElementById('last-name').value
-    const address = document.getElementById('address').value
-    const city = document.getElementById('city').value
-    const email = document.getElementById('email').value
+    const showError = (input, message) => {
+
+        const formField = input;
+        // ajoute message d'erreur
+        formField.classList.remove('success');
+        formField.classList.add('error');
+    
+        // montre le message
+        const error = formField.parentElement.querySelector('small');
+        error.textContent = message;
+    };
+
+    const showSuccess = (input) => {
+
+        const formField = input;
+    
+        // enlève la classe error
+        formField.classList.remove('error');
+        formField.classList.add('success');
+    
+        // cache le message d'erreur
+        const error = formField.parentElement.querySelector('small');
+        error.textContent = '';
+    };
+
+    const checkFirstName = () => {
+        const validName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
+        console.log(firstName.value);
+
+        if(validName.test(firstName.value) !== true) {
+            showError(firstName, 'Name error')
+        } else {
+            showSuccess(firstName);
+            return true;
+        }
+    };
+
+    const checkLastName = () => {
+        const validName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
+        console.log(lastName.value);
+
+        if(validName.test(lastName.value) !== true) {
+            showError(lastName, 'Name error')
+        } else {
+            showSuccess(lastName);
+            return true;
+        }
+    };
+
+    const checkCity = () => {
+        const validCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
+
+        if(validCity.test(city.value) !== true) {
+            showError(city, 'city error')
+        } else {
+            showSuccess(city);
+            return true;
+        }
+    };
+
+    const checkAddress = () => {
+        const validAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
+
+        if(validAddress.test(address.value) !== true) {
+            showError(address, 'city error')
+        } else {
+            showSuccess(address);
+            return true;
+        }
+    };
+
+    const checkEmail = () => {
+        const validMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
+
+        if(validMail.test(email.value) !== true) {
+            showError(email, 'city error')
+        } else {
+            showSuccess(email);
+            return true;
+        }
+    };
+
     let products = [];
 
-    const validName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
-    const validCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
-    const validMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
-    const validAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
+    if (checkCity() && checkFirstName() && checkLastName() && checkEmail() && checkAddress()) {
 
-    console.log(validName.test(firstName.value));
-
-    // if(validName.test(firstName.value) !== true) {
-    //     firstName.parentNode.insertBefore(formError, firstName.nextSibling);
-    //     firstName.classList.add('error');
-    // }
-    // // if()
-
-    if ((validName.test(firstName) == true) &&
-        (validName.test(lastName) == true) &&
-        (validAddress.test(address) == true) &&
-        (validCity.test(city) == true) &&
-        (validMail.test(email) == true)) {
-
-            for(let product in getCart()) {
-                products.push(product);
-            }
-        
-            const contact = {
-                contact : {
-                    firstName,
-                    lastName,
-                    address,
-                    city,
-                    email
-                },
-                products
-            };
-        
-            const response = await sendFormData('/api/teddies/order', contact);
-        
-            if (response) {
-                //vide le localstorage
-                localStorage.clear();
-                // //crée nouveau localstorage pour stocker les données de commande
-                let orderValidation = {
-                    orderId: response.orderId,
-                    totalCart: document.getElementById("total-price").innerText,
-                    firstName: response.contact.firstName,
-                    email: response.contact.email
-                };
-                localStorage.setItem("orderValidation", JSON.stringify(orderValidation));
-                window.location = '/ordervalidation';
-            }
-    }
-    //  else {
-    //     alert("Merci de renseigner correctement les champs du formulaire afin de valider votre commande.");
-    //     return false;
-    // }
+        for(let product in getCart()) {
+            products.push(product);
+        }
     
+        const contact = {
+            contact : {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                address: address.value,
+                city: city.value,
+                email: email.value
+            },
+            products
+        };
+    
+        const response = await sendFormData('/api/teddies/order', contact);
+        
+        if (response) {
+            //vide le localstorage
+            localStorage.clear();
+            // //crée nouveau localstorage pour stocker les données de commande
+            let orderValidation = {
+                orderId: response.orderId,
+                totalCart: document.getElementById("total-price").innerText,
+                firstName: response.contact.firstName,
+                email: response.contact.email
+            };
+            localStorage.setItem("orderValidation", JSON.stringify(orderValidation));
+            window.location = '/ordervalidation';
+        }
+    }
 }
